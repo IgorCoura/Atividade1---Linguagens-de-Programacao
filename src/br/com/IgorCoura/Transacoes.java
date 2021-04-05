8395483aa8f1e72e01e16df82d1c35d77e5718c6
Igor de Brito Coura <igor-coura@hotmail.com>
2021-04-04 21:04:16 -0300

    Update Transacoes.java

--- src/br/com/IgorCoura/Transacoes.java
+++ src/br/com/IgorCoura/Transacoes.java
@@ -13,15 +13,19 @@ public class Transacoes {
     }
 
     public static String gerarQRcode(Conta conta, double valor){
-        return conta.getIdConta() +";"+ conta.getUsuario().getNome() + ";"+ valor+";"+ getRandomNumberInRanger(1000, 9999);
+        return conta.getIdConta() +";"
+                +conta.getUsuario().getNome() + ";"
+                +valor+";"+ getRandomNumberInRanger(1000, 9999);
     }
 
     public static String pagamentoQRcode(String QRcode, Conta conta){
-        String[] dados = QRcode.split(";");                                             // Divide os dados
-        int id = Integer.parseInt(dados[0]);                                                  //transforma o id de string para inteiro
-        Conta contaDestino = ListaContas.getConta( (c)->{return  c.getIdConta() == id && c.getUsuario().getNome().equals(dados[1]);}); // Busca conta de acordo com id e nome igual ao do qrCode
-        if(contaDestino != null){                                                                //Caso a procura de conta não retorne nada o qrCode informado é invalido
-            if(conta.sacar(Double.parseDouble(dados[2]))){                                       //Tenta retira o dinheiro da conta pagadora e caso consiga deposita na conta destino.
+        String[] dados = QRcode.split(";");                                                 // Divide os dados
+        int id = Integer.parseInt(dados[0]);                                                      //transforma o id de string para inteiro
+        Conta contaDestino = ListaContas.getConta(
+                (c)->{return  c.getIdConta() == id && c.getUsuario().getNome().equals(dados[1]);} // Busca conta de acordo com id e nome igual ao do qrCode
+                );
+        if(contaDestino != null){                                                                 //Caso a procura de conta não retorne nada o qrCode informado é invalido
+            if(conta.sacar(Double.parseDouble(dados[2]))){                                        //Tenta retira o dinheiro da conta pagadora e caso consiga deposita na conta destino.
                 contaDestino.depositar(Double.parseDouble(dados[2]));
                 return "Pagamento de "+ dados[2] +" realizado com sucesso";
             }
